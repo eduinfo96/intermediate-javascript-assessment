2c511352f11876c1683f65b689949c3c0434a4ea
Theo Beck <eduinfo96@yahoo.com>
2016-12-12 03:27:24 -0600

    Finished

--- src/assessment.js
+++ src/assessment.js
@@ -50,7 +50,7 @@ var foo;
   which changes the variable foo (above) to 'bar'
   And resolve the promise when setTimeout completes.
 */
-function async(){
+ async = () => {
   let defer = $q.defer();
 
     setTimeout( () => {
@@ -67,10 +67,9 @@ function async(){
 // Write a function called context1 that takes in 4 parameters: A function called myFn, an object called context, param1, and param2.
 // Invoke myFn explicitly setting the context to the object called context.  Pass in param1 and param2 in order as well.
 
-function context1( myFn, context, param1, param2 ){
+context1 = ( myFn, context, param1, param2 ) => {
  return myFn.call( context, param1, param2 );
 }
-
 
 
 
@@ -79,7 +78,7 @@ function context1( myFn, context, param1, param2 ){
 // Write a function called context2 that takes in 3 parameters: A function called myFn, an object called context, and an array called params
 // Invoke myFn explicitly setting the context to the object called context.  Pass in params
 
-function context2( myFn, context, params ){
+context2 = ( myFn, context, params ) => {
   return myFn.apply( context, params );
 }
 
@@ -89,7 +88,7 @@ function context2( myFn, context, params ){
 // Write a function called context3 that takes in 2 parameters: A function called myFn, and an object called context
 // Make sure the function is permanently linked to the context.  This should give you a new function, return it.
 
-function context3( myFn, context ){
+context3 = ( myFn, context ) => {
   return myFn.bind( context );
 }
 
@@ -97,20 +96,35 @@ function context3( myFn, context ){
 // #6  ###################
 // # Constructor Function
 // Make a constructor function called taco that takes in 3 parameters: shell, meat, veggies and assigns them to identically named properties.
-
+function Taco( shell, meat, veggies ){
+  this.shell = shell
+  this.meat = meat
+  this.veggies = veggies
+}
 
 
 
 // #7  ###################
 // # Implicit binding
 // Make a constructor function called burrito.  It has a property called percentLeft = 100.  It has a property called eat that is a function.  When eat is invoked it uses context to implicitly subract 25 from the percentLeft on the burrito.
-
+function Burrito(){
+  this.percentLeft = 100;
+  this.eat = () => {
+    this.percentLeft = this.percentLeft - 25;
+  }
+}
 
 
 // #8  ###################
 // # Prototype 1
 // Add prototype function to the array type that doubles the value of every item in the array
 
+Array.prototype.doubler = function(){
+    for( var i = 0; i < this.length; i++ ){
+    this[i] = this[i] * 2;
+  }
+  return this;
+}
 
 
 
@@ -118,7 +132,12 @@ function context3( myFn, context ){
 // # Prototype 2
 // Write a constructor function called chimichanga.  It has a property called percentLeft = 100.  It has a prototype function called eat.  When eat is invoked it uses context to implicitly subract 20 from the precentLeft on the chimichanga.
 
-
+function Chimichanga(){
+  this.percentLeft = 100;
+  Chimichanga.prototype.eat = function(){
+    this.percentLeft = this.percentLeft - 20;
+  }
+}
 
 
 // #10  ###################
@@ -126,6 +145,14 @@ function context3( myFn, context ){
 // Write a function called sentence machine.  It takes in a parameter called partOne.  It returns a function called sentenceSmasher.
 // When sentenceSmasher is invoked it should take in a parameter called partTwo and return a new string that adds partOne and partTwo together.
 
+sentenceMachine = ( partOne ) => {
+
+  return sentenceSmasher = ( partTwo ) => {
+
+    return partOne + partTwo;
+  }
+
+}
 
 
 // #11  ###################
@@ -140,7 +167,19 @@ function context3( myFn, context ){
 //     ingredients: <Array of ingredients goes here>
 // }
 // ```
+subway = ( personName ) => {
 
+  let customer = {
+    orderPerson: personName
+    , ingredients: []
+  }
+
+  return addIngredient = ( ingredient ) => {
+    customer.ingredients.push( ingredient );
+
+    return customer;
+  }
+}
 
 
 // #12  ###################
@@ -149,3 +188,17 @@ function context3( myFn, context ){
 // If both parameters are the same type an the same value return "Exact match".
 // If both parameters have the same value but are different types return "Different types"
 // Otherwise return "Different values"
+
+compareValues = ( param1, param2 ) => {
+  if( param1 === param2 ){
+    return "Exact match";
+  }
+
+  else if( param1 == param2 ){
+    return "Different types"
+  }
+
+  else {
+    return "Different values"
+  }
+}
